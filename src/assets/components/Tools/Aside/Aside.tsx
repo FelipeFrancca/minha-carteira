c8f057fe3935af23031ffa494a3e144ff819e0d1
felipefrancca <suporte.felipefranca@gmail.com>
2023-05-31 06:37:36 -0300

    aside

--- src/assets/components/Tools/Aside/Aside.tsx
+++ src/assets/components/Tools/Aside/Aside.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { StyleConfig } from "./StyleConfig"; 
+import Logo from "../../../img/logo.png";
+import {
+    StyleConfig,
+    Header,
+    LogoImg,
+    Title,
+    Menu,
+    MenuItemLink
+} from "./StyleConfig"; 
 
 const Aside: React.FC = () => {
     return (
         <StyleConfig>
-            <h1>Aside</h1>
+            <Header>
+                <LogoImg src={Logo} alt="Logo minha carteira" />
+                <Title>Minha Carteira</Title>
+            </Header>
+
+            <Menu>
+                <MenuItemLink href="#">
+                    Dashboard
+                </MenuItemLink>
+            </Menu>
         </StyleConfig>
     )
 }
--- src/assets/components/Tools/Aside/StyleConfig.ts
+++ src/assets/components/Tools/Aside/StyleConfig.ts
@@ -1,7 +1,31 @@
 import styled from "styled-components";
 export const StyleConfig = styled.div`
     grid-area: AS;
-    color: ${(props) => props.theme.palette.white};
     background-color: ${props => props.theme.palette.secondary};
     border-right: solid 3px ${props => props.theme.palette.gray};
+    padding: 20px 20px;
+`;
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+`;
+
+export const LogoImg = styled.img`
+    width: 40px;
+    height: 40px;
+`;
+
+export const Title = styled.h3`
+color: ${(props) => props.theme.palette.white};
+margin-left: 10px;
+`;
+
+export const Menu = styled.nav`
+    margin-top: 100px;
+`;
+
+export const MenuItemLink = styled.a`
+    text-decoration: none;
+    color: ${(props) => props.theme.palette.white};
 `;
